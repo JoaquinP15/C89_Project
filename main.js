affbcfd6dc5ea4c453ec3d3783bba3584fa7ac82
JoaquinP15 <94157179+JoaquinP15@users.noreply.github.com>
2022-03-15 20:27:03 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function Login()
+{
+    player1 = document.getElementById("player1_name").value;
+    player2 = document.getElementById("player2_name").value;
+
+    localStorage.setItem("player1", player1);
+    localStorage.setItem("player2", player2);
+
+    window.location = "quiz_game_index.html"
+}
